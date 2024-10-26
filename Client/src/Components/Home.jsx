5ebbd6c76a63ccb2e77ea596ce26f10a652f3d53
Sanyam Pandey <sanyam071104@gmail.com>
2024-10-26 10:58:37 +0530

    main page completed

--- Client/src/Components/Home.jsx
+++ Client/src/Components/Home.jsx
@@ -3,12 +3,16 @@ import Navbar from '../shared/Navbar';
 import React from 'react'
 import Header from './header/Header';
 import Achievement from './Achievement/Achievement';
+import About from './about/About';
+import Services from './Servicess/Services';
 
 const Home = () => {
     return (<>
         <Navbar />
         <Header />
         <Achievement/>
+        <About/>
+        <Services/>
     </>
     )
 }
