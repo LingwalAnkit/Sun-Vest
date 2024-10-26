bbdb2652e9b4c8f8c34f344b73b8f29b36170b9d
Satyam296 <satyamchhetri629@gmail.com>
2024-10-26 12:11:32 +0530

    Homepage-edits

--- Client/src/Components/header/Header.jsx
+++ Client/src/Components/header/Header.jsx
@@ -14,10 +14,10 @@ const Header = () => {
             <div className='container full__height blur-effect'>
                 <div className="column">
                     <h1 className='title'>
-                        Revolutionary Home <span className="g-text">Solar System</span>
+                    Sunvest: Let the<span className="g-text"> Sun</span> Work for You and Your Community!
                     </h1>
                     <p className="text-muted">
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere veniam molestias nihil neque ad odit blanditiis explicabo? Quae ullam consequuntur fugiat quisquam excepturi. Blanditiis voluptate praesentium, in amet placeat minima?
+                    Sunvest is a platform that lets you invest in community solar projects, saving on energy bills while tracking your solar production and environmental impact. Join Sunvest to power your home with clean energy and contribute to a sustainable future.
                     </p>
                     <div className="buttons__container">
                         <Link to='services' className='btn'>Our Services</Link>
